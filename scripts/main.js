a0128515e7987525497d2e0af0a9b7e9e310d8c5
Nadya <kirillova202001@yandex.ru>
2020-08-04 16:54:40 +0300

    график

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,77 @@
 document.querySelector('button').onclick = function() {
     alert('А это еще не готово! Надо подождать...');
 }
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+//прямоугольник
+ctx.fillStyle = "rgba(245, 117, 224, 0.46)";
+ctx.fillRect(150, 180, 140, 70)
+
+//дуга
+ctx.moveTo(150, 180);
+ctx.arc(150, 180, 70, Math.PI, (Math.PI / 2), true);
+ctx.fill();
+
+//треугольник
+ctx.beginPath();
+ctx.moveTo(150, 40);
+ctx.lineTo(220, 180);
+ctx.lineTo(150, 180);
+ctx.lineTo(150, 40);
+ctx.fill();
+ctx.closePath();
+
+//оси
+ctx.fillStyle = "black";
+ctx.beginPath();
+ctx.moveTo(150,0);
+ctx.lineTo(150, 330);
+ctx.moveTo(0, 180)
+ctx.lineTo(330,180);
+ctx.closePath();
+ctx.stroke();
+
+//засечки
+ctx.beginPath();
+ctx.moveTo(147, 40);
+ctx.lineTo(153, 40);
+ctx.stroke();
+
+ctx.moveTo(147, 110);
+ctx.lineTo(153, 110);
+ctx.stroke();
+
+ctx.moveTo(220, 177);
+ctx.lineTo(220, 183);
+ctx.stroke();
+
+ctx.moveTo(290, 177);
+ctx.lineTo(290, 183);
+ctx.stroke();
+
+ctx.moveTo(147, 250);
+ctx.lineTo(153, 250);
+ctx.stroke();
+
+ctx.moveTo(147, 320);
+ctx.lineTo(153, 320);
+ctx.stroke();
+
+ctx.moveTo(80, 177);
+ctx.lineTo(80, 183);
+ctx.stroke();
+
+ctx.moveTo(10, 177);
+ctx.lineTo(10, 183);
+ctx.stroke();
+
+ctx.closePath();
+
+
+
+
+
+
+
