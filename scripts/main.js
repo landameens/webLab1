d81a5be86a05feb58b1653ce1a9316655ab5eeef
Nadya <kirillova202001@yandex.ru>
2020-08-05 00:26:31 +0300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,8 @@ document.querySelector('button').onclick = function() {
     alert('А это еще не готово! Надо подождать...');
 }
 
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 //прямоугольник
 ctx.fillStyle = "rgba(245, 117, 224, 0.46)";
